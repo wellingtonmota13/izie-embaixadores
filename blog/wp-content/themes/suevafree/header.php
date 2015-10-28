a55c8c60dbcf9faabfea63dc26fd8389fd44ab5f
Wellington <impeto.wellington@gmai.com>
2015-10-28 10:37:32 -0400

    add páginas extras

--- blog/wp-content/themes/suevafree/header.php
+++ blog/wp-content/themes/suevafree/header.php
@@ -10,6 +10,32 @@
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.2, user-scalable=yes" />
 
+<!-- Bootstrap Core CSS -->
+    <link href="inc/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+
+    <!-- Custom Fonts -->
+    <link href="inc/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,100,300,700,900' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <link href="inc/css/iziestyle.css" rel="stylesheet">
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
 <!--[if IE 8]>
     <script src="<?php echo get_template_directory_uri(); ?>/inc/scripts/html5shiv.js" type="text/javascript"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/inc/scripts/selectivizr-min.js" type="text/javascript"></script>
@@ -21,13 +47,16 @@
 
 <body <?php body_class(); ?>>
 
+
 <div id="wrapper">
+
 
     <header class="header container" >
     
         <div class="row">
         
             <div class="span12" >
+            
             
                 <div id="logo">
                         
