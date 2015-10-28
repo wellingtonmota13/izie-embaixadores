b399cdd7c65037c3a603c8fe7376cd46b2fa00cc
Wellington <impeto.wellington@gmai.com>
2015-10-28 11:18:07 -0400

    alerações no blog

--- blog/wp-content/themes/suevafree/footer.php
+++ blog/wp-content/themes/suevafree/footer.php
@@ -1,64 +1,65 @@
-    <footer id="footer">
-    
-        <div class="container">
-        
-            <?php if ( is_active_sidebar('bottom-sidebar-area') ) : ?>
-            
-                <!-- FOOTER WIDGET BEGINS -->
-                
-                    <section class="row widget">
-                        <?php dynamic_sidebar('bottom-sidebar-area') ?>
-                    </section>
-                    
-                <!-- FOOTER WIDGET END -->
-                
-            <?php endif; ?>
-    
-             <div class="row copyright" >
-               
-                <div class="span6" >
-                
-                    <p>
-                        <?php if (suevafree_setting('suevafree_copyright_text')): ?>
-                           <?php echo esc_html(suevafree_setting('suevafree_copyright_text')); ?>
-                        <?php else: ?>
-                          <?php _e('Copyright','suevafree'); ?> <?php echo get_bloginfo("name"); ?> <?php echo date("Y"); ?> 
-                        <?php endif; ?>
-        
-                    </p>
-                    
-                </div>
-                
-                <div class="span6" >
-                
-                    <!-- start social -->
-                    
-                    <div class="socials">
-                    
-                        <?php do_action('suevafree_socials'); ?>
-                   
+<!-- Footer -->
+
+        <footer class="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col col-md-5">
+                        <h5>Info</h5>
+                        <p><a href="#">Blog</a><br>
+                        <p><a href="#">Imprensa</a><br>
+                        <p><a href="#">Termos de uso</a><br>
+                        <p><a href="#">Política de privacidade</a><br>
+                        </p>
                     </div>
-                    
-                    <!-- end social -->
-    
+                    <div class="footer-col col-md-4">
+                        <h5>Fique por perto</h5>
+                        <ul class="footer-share">
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="https://twitter.com/kamal_chaneman"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="https://www.linkedin.com/in/kamalchaneman"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col col-md-3">
+                        <h5>Enteda mais</h5>
+                        <p><a href="#">Embaixadores da Marca</a><br>
+                        <p><a href="#">Perguntas frequentes</a><br>
+                        </p>
+                        
+                    </div>
                 </div>
-                
             </div>
-            
+        </div><!-- footer top -->
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="col-md-12">
+                    <p>Copyright © 2015 Izie S/A. All Rights Reserved<br>Feito com <i class="fa fa-heart pulse"></i> por nós.</p>
+                </div>
+            </div>
         </div>
-        
     </footer>
-    
-</div>
 
-    <?php if ( (!suevafree_setting('suevafree_view_back_to_top') ) || ( suevafree_setting('suevafree_view_back_to_top') == "on" ) ): ?>
-    
-        <div id="back-to-top"> <i class="icon icon-chevron-up"></i> </div>
-	
-    <?php endif; ?>
+
     
 
-<?php wp_footer() ?>   
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/agency.js"></script>
 
 </body>
 
